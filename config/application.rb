bc604b8bfa7f3fc1dcd6b22bc0483d24b7b9211a
John W. Fail <john@pixelache.ac>
2016-08-31 01:55:20 +0300

    force utf8

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Temporary
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.encoding = "utf-8"
   end
 end
