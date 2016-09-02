151f8ac29cbc9b58d6ecb472d716d3c5fdca5371
John W. Fail <john@pixelache.ac>
2016-09-03 02:22:40 +0300

    mobile layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @news = Post.published.order(published_at: :desc)
     @open_day = Experiment.friendly.find('open-days').instances.future.first
     # @next_other = Experiment
-    @recent_proposal = Proposal.order(created_at: :desc).first
+    @recent_proposals = Proposal.all.order(created_at: :desc).to_a.delete_if{|x| x.scheduled? }
   end
   
 end
