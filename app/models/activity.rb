6cba67120783e4bb6660210b4160a7b929e6cdfb
John W. Fail <john@pixelache.ac>
2016-10-06 12:00:57 +0300

    improved activity feed

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,7 @@ class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :ethtransaction
   belongs_to :item, polymorphic: true
+  belongs_to :extra, polymorphic: true
   belongs_to :onetimer
   has_one :instances_user
   validates_presence_of :item_id, :item_type
