ed452748c997f32379661243ca03276d700e8e9f
John W. Fail <john@pixelache.ac>
2016-10-18 15:01:49 +0300

    force facebook callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret']
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], callback_url: 'https://temporary.fi/users/auth/facebook/callback',
   provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"]
   provider :github, ENV['github_key'], ENV['github_secret']
