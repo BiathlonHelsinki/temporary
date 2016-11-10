0992caec2518f8b04ff72ba0f6c35493cd5b649e
John W. Fail <john@pixelache.ac>
2016-11-10 13:40:50 +0200

    rsvp info displayed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], callback_url: 'https://temporary.fi/users/auth/facebook/callback'
-  provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
+  provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret'], callback_url: 'https://temporary.fi/users/auth/twitter/callback'
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"]
   provider :github, ENV['github_key'], ENV['github_secret']
 end
