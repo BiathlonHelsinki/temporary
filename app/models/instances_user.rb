95f4bb48f9a4b638d205b2ef356b2f1fe01d8e06
John W. Fail <john@pixelache.ac>
2016-11-30 14:17:28 +0200

    secure http

--- app/models/instances_user.rb
+++ app/models/instances_user.rb
@@ -3,5 +3,5 @@ class InstancesUser < ApplicationRecord
   belongs_to :user
   belongs_to :activity
   validates_presence_of :instance_id, :activity_id, :user_id, :visit_date
-
+  validates_uniqueness_of :user_id, scope: [:instance_id, :visit_date]
 end
