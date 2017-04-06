3afb47988b546876fd0e146fe0e308ef021469e3
John W. Fail <john@pixelache.ac>
2017-04-06 14:37:37 +0300

    do not break pages if not logged in

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,8 @@
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.default_locale = :en
-I18n.available_locales = [:en]
-I18n.fallbacks[:en] = [:en]
+I18n.available_locales = [:en, :fi]
+I18n.fallbacks[:en] = [:en, :fi]
 
+I18n.fallbacks[:fi] = [:fi, :en]
 
-Globalize.fallbacks = {:en => [:en] }
+Globalize.fallbacks = {:en => [:en], :fi => [:fi] }
