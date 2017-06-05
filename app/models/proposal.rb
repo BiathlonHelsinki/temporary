7fe03221bb34dad7b5534477dea5377f3114d36e
John W. Fail <john@pixelache.ac>
2017-06-05 17:42:49 +0300

    pledge error

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -382,7 +382,7 @@ class Proposal < ApplicationRecord
           if intended_sessions == 0
             user.available_balance + (pledges.unconverted.find_by(user: user).nil? ? 0 :  pledges.unconverted.find_by(user: user).pledge)
           else
-            (user.available_balance + pledges.unconverted.find_by(user: user).pledge) > cumulative_needed_for(intended_sessions) ? cumulative_needed_for(intended_sessions) : (user.available_balance + pledges.unconverted.find_by(user: user).pledge) 
+            (user.available_balance + (pledges.unconverted.find_by(user: user).nil? ? 0 : pledges.unconverted.find_by(user: user).pledge)) > cumulative_needed_for(intended_sessions) ? cumulative_needed_for(intended_sessions) : (user.available_balance + pledges.unconverted.find_by(user: user).pledge) 
           end
         else
           pledges.unconverted.find_by(user: user).pledge
