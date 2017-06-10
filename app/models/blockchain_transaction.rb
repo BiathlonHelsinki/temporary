571ac29acd84908e551079e99fa871f0cda28191
John W. Fail <john@pixelache.ac>
2017-06-10 14:42:09 +0300

    make work without blockchain queue branch

--- app/models/blockchain_transaction.rb
+++ app/models/blockchain_transaction.rb
@@ -1,6 +1,6 @@
-class BlockchainTransaction < ApplicationRecord
-  belongs_to :transaction_type
-  belongs_to :account
-  belongs_to :ethtransaction
-  belongs_to :activity
-end
+# class BlockchainTransaction < ApplicationRecord
+#   belongs_to :transaction_type
+#   belongs_to :account
+#   belongs_to :ethtransaction
+#   belongs_to :activity
+# end
