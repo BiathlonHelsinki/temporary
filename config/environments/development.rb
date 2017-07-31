607d0845b88c48edac4ae15d513fd94c56a45afa
John W. Fail <john@pixelache.ac>
2017-07-31 16:30:44 +0300

    removed july banner

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,12 +54,12 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.after_initialize do
-    Bullet.enable = true
-    # Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
+    # Bullet.enable = true
+    # # Bullet.alert = true
+    # Bullet.bullet_logger = true
+    # Bullet.console = true
+    # Bullet.rails_logger = true
+    # Bullet.add_footer = true
   end
 end
 BetterErrors::Middleware.allow_ip! '192.168.11.20'
