3775640ec17dd9397ae489e9e9cfc72e2ef2060c
John W. Fail <john@ptarmigan.fi>
2024-03-21 20:41:53 +0200

    Try to enable caching

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require 'sprockets/railtie'
 require 'rails/all'
 require 'net/ping/tcp'
 
@@ -12,13 +12,14 @@ module Temporary
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults(7.1)
     config.encoding = "utf-8"
     config.middleware.insert(0, Rack::UTF8Sanitizer)
+    config.assets.precompile += %w[404.html 422.html 500.html]
     config.time_zone = 'Helsinki'
     config.action_view.embed_authenticity_token_in_remote_forms = true
      # config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_types = %i[datetime time]
-    config.cache_store = :memory_store
     config.hosts << 'temporary.fi'
     config.hosts << 'www.temporary.fi'
     #, "redis://localhost:6379/0/cache", { expires_in: 8.hours }
