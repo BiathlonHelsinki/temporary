d29f79e27ea71deb7362fccf816f1eb61fc43be8
John W. Fail <john@ptarmigan.fi>
2024-03-21 19:47:41 +0200

    Add hosts

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,14 @@ module Temporary
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.encoding = "utf-8"
-    config.middleware.insert 0, Rack::UTF8Sanitizer
+    config.middleware.insert(0, Rack::UTF8Sanitizer)
     config.time_zone = 'Helsinki'
     config.action_view.embed_authenticity_token_in_remote_forms = true
      # config.active_record.default_timezone = :local
-    config.active_record.time_zone_aware_types = [:datetime, :time]
+    config.active_record.time_zone_aware_types = %i[datetime time]
     config.cache_store = :memory_store
+    config.hosts << 'temporary.fi'
+    config.hosts << 'www.temporary.fi'
     #, "redis://localhost:6379/0/cache", { expires_in: 8.hours }
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
   end
